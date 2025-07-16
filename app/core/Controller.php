28ef1d2b9c7414a9e9642bd723ec9910749b00f1
LKartavya <147118099+LKartavya@users.noreply.github.com>
2025-07-16 21:51:57 +0000

    Finished the work

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,14 +1,15 @@
 <?php
 
 class Controller {
-    
     public function model ($model) {
         require_once 'app/models/' .$model . '.php';
         return new $model();
     }
-    
-    public function view ($view, $data = []) {
-        require_once 'app/views/' . $view .'.php';
-    }
 
+    public function view($view, $data = []){
+        if (!empty($data)) {
+            extract($data);
+        }
+        require_once 'app/views/' . $view . '.php';
+    }
 }
